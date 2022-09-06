150af6c2e9e939aa774744f1254caa5a69b7c467
suabae <bsa0322@ewhain.net>
2022-09-07 01:55:46 +0900

    정수론 3036

--- number-theory/3036.cpp
+++ number-theory/3036.cpp
@@ -2,3 +2,29 @@
 // Created by BaeSuA on 2022-09-07.
 //
 
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    return gcd(b, a % b);
+}
+
+int main() {
+    int n;
+
+    cin >> n;
+    vector<int> ring(n, 0);
+    for (int i = 0; i < n; i++) {
+        cin >> ring[i];
+    }
+    for (int i = 1; i < n; i++) {
+        int g = gcd(ring[0], ring[i]);
+        cout << ring[0] / g << '/' << ring[i] / g << '\n';
+    }
+    return 0;
+}
