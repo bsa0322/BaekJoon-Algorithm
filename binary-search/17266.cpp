f01a345e76d2e24124ab1431223f17302bb6033f
sua bae <bsa0322@ewhain.net>
2022-10-06 04:56:40 +0900

    17266 update

--- binary-search/17266.cpp
+++ binary-search/17266.cpp
@@ -15,7 +15,6 @@
 using namespace std;
 
 bool isPossible(int n, int height, vector<int> &lamp) {
-    vector<bool> visited(n + 1, false);
     int cur = 0;
     for (int i = 0; i < lamp.size(); i++) {
         if (cur < lamp[i] - height) { //지난 구간의 마지막을 이어서 커버하지 못하면 바로 false
