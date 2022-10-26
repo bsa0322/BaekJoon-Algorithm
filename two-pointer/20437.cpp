3220a004610efd9d542627fef4b3154f2cebd11a
sua bae <bsa0322@ewhain.net>
2022-10-26 18:10:30 +0900

    [two-pointer] 20437

--- two-pointer/20437.cpp
+++ two-pointer/20437.cpp
@@ -8,30 +8,81 @@
 using namespace std;
 const int INF = 1e4;
 
-pair<int, int> getString(string w, int k) {
-    int left = 0;
-    int right = 0;
+int getMin(string w, int k) {
+    int left = 0, right = 0, ans = INF;
     vector<int> alpha(26, 0);
-    int ans1 = INF, ans2 = 0;
-    alpha[w[right] - 'a']++;
+    alpha[w[0] - 'a']++;
     while (left <= right && right < w.length()) {
-        int left_num = w[left] - 'a';
-        int right_num = w[right] - 'a';
-
-        if (alpha[right_num] == k) {
-            int leng = right - left + 1;
-            ans1 = min(ans1, leng);
-            if (w[left] == w[right]) {
-                ans2 = max(ans2, leng);
-            }
-            alpha[left_num]--;
+        if (alpha[w[right] - 'a'] == k) {
+            ans = min(ans, right - left + 1);
+            alpha[w[left] - 'a']--;
             left++;
-        } else if (alpha[right_num] < k) {
+        } else if (alpha[w[right] - 'a'] < k) {
             right++;
+            if (right == w.length()) {
+                break;
+            }
             alpha[w[right] - 'a']++;
         }
     }
-    return {ans1, ans2};
+    return ans;
+}
+
+/**
+ * 일단 둘 다, 왼쪽에서 시작
+ * 최대 k인 첫번째 찾으면 left, right 둘 다 옮김
+ * 아직 k가 안되면 right 포인터 이동
+ * k이면서 왼쪽 오른쪽 같다면 정답 갱신
+ *
+ */
+/**
+ * 반례
+ * 1
+superaauktornkao
+2
+ 지금 문자열에 포함된 개수보다 아직 K만큼이 남았다면 오른쪽으로 포인터 이동
+ 이때 기준은 left문자
+
+ 해결했는데도 5%에서 틀렸습니다..
+
+ */
+int getMax(vector<int> &default_alpha, string w, int k) {
+    int left = 0, right = 0, ans = 0;
+    vector<int> alpha(26, 0); //부분 문자열에 속한 알파벳 개수
+    alpha[w[0] - 'a']++;
+    default_alpha[w[0] - 'a']--;
+    while (left <= right && right < w.length()) {
+//        cout << w[left] << ' ' << w[right] << ' ' << ans << '\n';
+        if (alpha[w[left] - 'a'] == k && w[left] == w[right]) {
+            ans = max(ans, right - left + 1);
+            right++;
+            if (right == w.length()) {
+                break;
+            }
+            alpha[w[right] - 'a']++;
+            default_alpha[w[right] - 'a']--;
+            alpha[w[left++] - 'a']--;
+        } else if (default_alpha[w[left] - 'a'] > 0 && k - alpha[w[left] - 'a'] > 0 &&
+                   k - alpha[w[left] - 'a'] <= default_alpha[w[left] - 'a']) {
+            right++;
+            if (right == w.length()) {
+                break;
+            }
+            alpha[w[right] - 'a']++;
+            default_alpha[w[right] - 'a']--;
+        } else if (alpha[w[left] - 'a'] > k) {
+            alpha[w[left++] - 'a']--;
+        } else {
+            right++;
+            if (right == w.length()) {
+                break;
+            }
+            alpha[w[right] - 'a']++;
+            default_alpha[w[right] - 'a']--;
+            alpha[w[left++] - 'a']--;
+        }
+    }
+    return ans;
 }
 
 /**
@@ -49,12 +100,17 @@ int main() {
     cin >> t;
     while (t--) {
         cin >> w >> k;
-        pair<int, int> ans = getString(w, k);
+        vector<int> default_alpha(26, 0);
+        for (int i = 0; i < w.length(); i++) {
+            default_alpha[w[i] - 'a']++;
+        }
+        int ans1 = getMin(w, k);
+        int ans2 = getMax(default_alpha, w, k);
 
-        if (ans.first == INF) {
+        if (ans1 == INF) {
             cout << "-1\n";
         } else {
-            cout << ans.first << ' ' << ans.second << '\n';
+            cout << ans1 << ' ' << ans2 << '\n';
         }
 
     }
