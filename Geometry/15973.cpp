dac8b5ea0e7d1c4cc3e702c5ea006c538bce20dd
suabae <bsa0322@ewhain.net>
2024-09-18 23:50:59 +0900

    [ADD] 15973 (math)

--- Geometry/15973.cpp
+++ Geometry/15973.cpp
@@ -2,43 +2,69 @@
 
 using namespace std;
 
-bool isFace(int x, int y, int x1, int y1, int x2, int y2) {
-    if (x > x1 && x < x2 && y < y2)
+bool isPoint(int &x1, int &y1, int &x2, int &y2, int &x3, int &y3, int &x4, int &y4) {
+    if (x2 == x3 && y2 == y3) { // 오른쪽 위
         return true;
-    else if (y > y1 && y < y2 && x < x1)
+    }
+    if (x1 == x4 && y2 == y3) { // 왼쪽 위
         return true;
+    }
+    if (x1 == x4 && y1 == y4) { // 왼쪽 아래
+        return true;
+    }
+    if (x2 == x3 && y1 == y4) { // 오른쪽 아래
+        return true;
+    }
     return false;
 }
 
-bool isLine(int x1, int y1, int x2, int y2) {
-    if (x1 == x2 || y1 == y2)
+bool isLine(int &x1, int &y1, int &x2, int &y2, int &x3, int &y3, int &x4, int &y4) {
+    if (x2 == x3 && y3 <= y2 && y4 >= y1) { // 오른쪽
         return true;
+    }
+    if (y2 == y3 && x3 <= x2 && x4 >= x1) { // 위쪽
+        return true;
+    }
+    if (x1 == x4 && y3 <= y2 && y4 >= y1) { // 왼쪽
+        return true;
+    }
+    if (y1 == y4 && x3 <= x2 && x4 >= x1) { // 아래쪽
+        return true;
+    }
     return false;
 }
 
-bool isPoint(int x1, int y1, int x2, int y2) {
-    if (x1 == x2 && y1 == y2)
+bool isFace(int &x1, int &y1, int &x2, int &y2, int &x3, int &y3, int &x4, int &y4) {
+    if ((x4 >= x1 && x4 <= x2) || (x3 >= x1 && x3 <= x2)) { // 세로
+        if (y4 >= y1 && y3 <= y2) {
+            return true;
+        }
+    }
+    if ((y4 >= y1 && y4 <= y2) || (y3 >= y1 && y3 <= y2)) { // 가로
+        if (x4 >= x1 && x3 <= x2) {
+            return true;
+        }
+    }
+    if (x1 >= x3 && x4 >= x2 && y1 >= y3 && y4 >= y2) { // 바깥에
         return true;
+    }
+    if (x1 <= x3 && x4 <= x2 && y1 <= y3 && y4 <= y2) { // 안에
+        return true;
+    }
     return false;
 }
 
 int main() {
-    int x1, y1, x2, y2; //박스1
-    int a1, b1, a2, b2; //박스2
-
-    //입력
-    cin >> x1 >> y1 >> x2 >> y2;
-    cin >> a1 >> b1 >> a2 >> b2;
-
-    //연산
-    if (isPoint(x2, y2, a1, b1) || isPoint(x1, y2, a2, b1) || isPoint(x1, y1, a2, b2) || isPoint(x2, y1, a1, b2))
-        cout << "POINT\n";
-    else if (isLine(x2, y2, a1, b1) || isLine(x1, y2, a2, b1) || isLine(x1, y1, a2, b2) || isLine(x2, y1, a1, b2))
-        cout << "LINE\n";
-    else if (isFace(a1, b1, x1, y1, x2, y2) || isFace(a2, b1, x1, y1, x2, y2) || isFace(a2, b2, x1, y1, x2, y2) ||
-             isFace(a1, b2, x1, y1, x2, y2))
-        cout << "FACE\n";
-    else
-        cout << "NULL\n";
+    int x1, y1, x2, y2, x3, y3, x4, y4;
+    cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
+    if (isPoint(x1, y1, x2, y2, x3, y3, x4, y4)) {
+        cout << "POINT";
+    } else if (isLine(x1, y1, x2, y2, x3, y3, x4, y4)) {
+        cout << "LINE";
+    } else if (isFace(x1, y1, x2, y2, x3, y3, x4, y4)) {
+        cout << "FACE";
+    } else {
+        cout << "NULL";
+    }
     return 0;
 }
