7a3214e9e6ec7729196a1dde92b790edbd701966
Sua Bae <68186101+bsa0322@users.noreply.github.com>
2024-12-12 22:46:29 +0900

    [Bronze V] Title: 가희와 4시간의 벽 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32775. 가희와 4시간의 벽 1/가희와 4시간의 벽 1.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if (a <= b) {
+        cout << "high speed rail";
+    } else {
+        cout << "flight";
+    }
+    return 0;
+}
